9bec499585a3484a043aa1f50a5a181a99a01165
iwswordpress <iwswordpress@gmail.com>
2020-10-05 16:27:37 +0100

    SET up async SAGA

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-// import createSagaMiddleware from 'redux-saga';
 import './index.css';
 import Counter from './Counter';
 import reducer from './reducers';
 
-const store = createStore(reducer);
-const action = type => store.dispatch({ type });
+// add as per 1.1
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+// ...
+import rootSaga from './sagas';
 
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
+
+const action = type => store.dispatch({ type });
+// end
 function render() {
   ReactDOM.render(
     <div className='container'>
--- src/sagas.js
+++ src/sagas.js
@@ -1,19 +1,22 @@
-import { all, call, delay, put, takeEvery } from 'redux-saga/effects';
+import { put, takeEvery, all } from 'redux-saga/effects';
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 export function* helloSaga() {
-  console.log('Hello Saga!');
+  yield console.log('Hello Sagas!');
 }
 
-export function* incrementAsync() {
+function* incrementAsync() {
   yield delay(1000);
   yield put({ type: 'INCREMENT' });
 }
 
-export function* watchIncrementAsync() {
+function* watchIncrementAsync() {
   yield takeEvery('INCREMENT_ASYNC', incrementAsync);
 }
 
+// notice how we now only export the rootSaga
 // single entry point to start all Sagas at once
 export default function* rootSaga() {
-  yield all([call(helloSaga), call(watchIncrementAsync)]);
+  yield all([helloSaga(), watchIncrementAsync()]);
 }
