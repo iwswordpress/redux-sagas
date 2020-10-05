f98ac76bb0a5910c9885bb30ec102c3c757d4a56
iwswordpress <iwswordpress@gmail.com>
2020-10-05 16:01:49 +0100

    REPLICATE TUTORIAL

---
+++ src/Counter.js
@@ -0,0 +1,13 @@
+/*eslint-disable no-unused-vars */
+import React from 'react';
+
+const Counter = ({ value, onIncrement, onDecrement }) => (
+  <div>
+    <button onClick={onIncrement}>Increment</button>{' '}
+    <button onClick={onDecrement}>Decrement</button>
+    <hr />
+    <div>Clicked: {value} times</div>
+  </div>
+);
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+// import createSagaMiddleware from 'redux-saga';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Counter from './Counter';
+import reducer from './reducers';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(reducer);
+const action = type => store.dispatch({ type });
+
+function render() {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => action('INCREMENT')}
+      onDecrement={() => action('DECREMENT')}
+      onIncrementAsync={() => action('INCREMENT_ASYNC')}
+    />,
+    document.getElementById('root')
+  );
+}
+
+render();
+store.subscribe(render);
