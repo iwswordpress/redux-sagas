011d953bcb65fec95cc39005969a7454ef0eb86c
iwswordpress <iwswordpress@gmail.com>
2020-10-06 01:03:55 +0100

    Changing store

--- src/Counter.js
+++ src/Counter.js
@@ -1,9 +1,9 @@
 import React from 'react';
 const Counter = ({ value, onDecrementAsync, onGetUsers, onIncrementAsync }) => (
   <div>
-    <button onClick={onDecrementAsync}>Increment</button>{' '}
-    <button onClick={onIncrementAsync}>Decrement</button>{' '}
-    <button onClick={onGetUsers}>GET USERS</button>
+    <button onClick={onDecrementAsync}>ASYNC Increment</button>{' '}
+    <button onClick={onIncrementAsync}>ASYNC Decrement</button>{' '}
+    <button onClick={onGetUsers}>ASYNC GET USERS</button>
     <hr />
     <div>Clicked: {value} times</div>
   </div>
