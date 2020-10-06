f83b67ddc2d834e5e008855322d98f4c1448fe6a
iwswordpress <iwswordpress@gmail.com>
2020-10-06 02:40:21 +0100

    Restructure app for chapter 3

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ We load the store with the counter and the users.
 
 We split reducer into two files and locate in a folder: store.
 
+We add an actions file.
+
 We use combineReducers in index.js
 
 We also ouput users as JSON.stringify for debug rather than presentation.
--- src/reducers.js
+++ src/reducers.js
@@ -1,3 +1,5 @@
+import * as actions from './store/actions';
+
 const initialState = {
   counter: 0,
   users: []
@@ -5,19 +7,19 @@ const initialState = {
 
 export default function counter(state = initialState, action) {
   switch (action.type) {
-    case 'INCREMENT':
+    case actions.INCREMENT:
       return { ...state, counter: state.counter + 1 };
-    case 'DECREMENT':
+    case actions.DECREMENT:
       return { ...state, counter: state.counter - 1 };
-    case 'SHOW_CONGRATULATION':
+    case actions.SHOW_CONGRATULATION:
       console.log('WELL DONE! 3 INCREMENTS');
       return state;
-    case 'FETCH_SUCCEEDED':
+    case actions.FETCH_SUCCEEDED:
       console.log('[REDUCER] FETCH_SUCCEEDED');
       console.log('[REDUCER] ACTION: ', action.type);
       console.log('[REDUCER] PAYLOAD: ', action.payload);
       return { ...state, users: action.payload };
-    case 'FETCH_FAILED':
+    case actions.FETCH_SUCCEEDED:
       console.log('[REDUCER] FETCH_FAILED');
       return state;
     default:
